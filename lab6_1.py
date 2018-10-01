c7cd350799be23a4342a5f8c64aad97ced7ddb4d
TarSen99 <31731917+TarSen99@users.noreply.github.com>
2018-10-01 10:58:44 +0300

    Update lab6_1.py

--- lab6_1.py
+++ lab6_1.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-a = int(input("Enter side a "))
-b = int(input("Enter side b "))
-c = int(input("Enter side c "))
+def sidesInput() -> list:
+    """This function read 3 sides of triangle and return list of them"""
+    a = float(input("Enter side a "))
+    b = float(input("Enter side b "))
+    c = float(input("Enter side c "))
+    return [a, b, c]
 
-def findArea(a: int, b: int, c: int) -> float:
-	"""This function gets 3 sides of triangle and calc area"""
-	halfOfPerimeter = (a + b + c) / 2
-	s = (halfOfPerimeter * (halfOfPerimeter - a) * (halfOfPerimeter - b) * (halfOfPerimeter - c))**(1/2)
-	return s
+def areaOutput(area: float):
+    """This function gets value of area and print it"""
+    print("The area of triangle is " + str(area))
 
-area = findArea(a, b, c)
-print("The area of triangle is " + str(area))
+def findArea(sides: list) -> float:
+    """This function gets list of sides and calc area"""
+    halfOfPerimeter = (sides[0] + sides[1] + sides[2]) / 2
+    s = (halfOfPerimeter * (halfOfPerimeter - sides[0]) * (halfOfPerimeter - sides[1]) * 
+    (halfOfPerimeter - sides[2]))**(1/2)
+    return s
+
+areaOutput(findArea(sidesInput()))
