77798116b2feb59cf1c52e07b6d926434899a2b5
TarSen99 <31731917+TarSen99@users.noreply.github.com>
2019-01-18 20:57:52 +0200

    Create 15_1_client.py

---
+++ 15_1_client.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python3
+
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = '127.0.0.1'
+port = 9999
+s.connect((host, port))
+server_response = s.recv(1024)
+print(server_response.decode('utf-8'))
+a = input()
+s.send(a.encode('utf-8'))
+server_response = s.recv(1024)
+print(server_response.decode('utf-8'))
+while True:
+    a = input()
+    s.send(a.encode('utf-8'))
+    if a == "Exit":
+        break
+    print(s.recv(1024).decode('utf-8'))
+s.close()
